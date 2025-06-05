257bd9f0dcb9f5dcc2e115b6a135e33cb8a587fb
Shakir <shakirrahman81@gmail.com>
2025-06-05 18:48:20 -0400

    Adds decrypt.py

---
+++ decrypt.py
@@ -0,0 +1,32 @@
+from xor_helpers import generate_xor_labels, generate_xor_slices, potential_match
+
+
+def auto_crib_drag(words, xor_data, len_ct, num_ct, dict):
+    """
+    Automatically crib drags words over the XOR'd ciphertexts.
+    There are three scenarios we could come across during this,
+    either the program.
+        a. deciphers entire words in the plaintexts.
+        b. deciphers portions of words in the plaintexts.
+        c. yields complete gibberish.
+    """
+
+    labels = generate_xor_labels(xor_data)
+    matches = []
+
+    for word in sorted(words):
+        crib = word.encode("utf-8")
+        crib_len = len(crib)
+        max_offset = len_ct - crib_len + 1  # +1 since range is exclusive
+
+        # # Debugging purposes
+        # print(f"Crib dragging '{crib}' across {labels}")
+
+        for offset in range(max_offset):
+            xor_slices = generate_xor_slices(xor_data, offset, crib_len)
+            matches_found = potential_match(xor_slices, crib, offset, dict)
+            for match in matches_found:
+                matches.append(match)
+    print("Finished looking for potential matches!")
+    print(f"Found {len(matches)} potential matches!")
+    return len(matches)
